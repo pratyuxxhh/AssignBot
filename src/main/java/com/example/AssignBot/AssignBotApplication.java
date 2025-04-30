776315798b29b7ea93ced9d325920f26cd407640
= <ishu.kool2406@gmail.com>
2025-04-30 13:58:48 +0530

    frontend is almost done

---
+++ src/main/java/com/example/AssignBot/AssignBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.AssignBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignBotApplication.class, args);
+	}
+
+}
