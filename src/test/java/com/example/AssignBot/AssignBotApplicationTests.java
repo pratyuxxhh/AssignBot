97b26e4cafcd9e8897f87b44d4126072091b7914
= <ishu.kool2406@gmail.com>
2025-04-30 13:58:48 +0530

    setting up files

---
+++ src/test/java/com/example/AssignBot/AssignBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AssignBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
